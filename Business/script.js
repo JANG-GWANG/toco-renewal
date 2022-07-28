904dbe1db7f162fbfa13db116be12ae3065cebe8
jino030 <105294951+jino030@users.noreply.github.com>
2022-07-28 21:42:33 +0900

    Add files via upload

--- Business/script.js
+++ Business/script.js
@@ -1,10 +1,63 @@
-// 슬릭슬라이더
+// 메인페이지 메뉴더보기 클릭시 디자인 변경
+$('#menuicon').on('click', function() {
+    if ( $(this).prop('checked') ) {
+        // 메뉴더보기 열릴 때
+        $('.nav-gnb').css({'background-color':'#fff', 'border-bottom':'1px solid #E9E9E9', 'box-sizing':"border-box"});
+        $('.menu-text').css({'color':'#000'});
+        $('.menuicon').css({'background-color':'#000'});
+        $('.menuicon-color').css({'background-color':'#000'});
+        document.getElementById("main-logo").src="./img/toco-common/logo-Resoft.svg";
+        $('.menu-text').hover(function(){
+            $(this).css('color','#184B9F');
+        }, function(){
+            $(this).css('color','#000');
+        });
+    } else {
+        // 메뉴더보기 닫힌 후
+        $('.nav-gnb').css({'background-color':'','border-bottom':'','transition':''});
+        $('.menu-text').css({'color':''});
+        $('.menuicon').css({'background-color':''});
+        $('.menuicon-color').css({'background-color':'', 'transition':'all .35s'});
+        document.getElementById("main-logo").src="./img/toco-common/logo-Resoft-white.png";
+        $('.menu-text').hover(function(){
+            $(this).css('color','#184B9F');
+        }, function(){
+            $(this).css('color','#fff');
+        });
+    }
+  });
+
+
+
+// 다른 페이지 메뉴 마우스 올렸을 때 디자인
+$('#page-menuicon').on('click', function() {
+    if ( $(this).prop('checked') ) {
+        // 메뉴더보기 열릴 때
+        $('.nav-gnb').css({'border-bottom':'1px solid #E9E9E9', 'box-sizing':"border-box"});
+        $('.page-menu-text').hover(function(){
+            $(this).css('color','#184B9F');
+        }, function(){
+            $(this).css('color','#000');
+        });
+    } else {
+        // 메뉴더보기 닫힌 후
+        $('.page-menu-text').hover(function(){
+            $(this).css('color','#184B9F');
+        }, function(){
+            $(this).css('color','#fff');
+        });
+    }
+  });
+
+
+
+// 사업현황페이지 슬릭슬라이더
 $(function(){
     $(".etc-projects").slick({
         slide: "div",                   // 슬라이드 되어야 할 태그
         infinite : true,                // 무한 반복 옵션     
         slidesToShow : 5,               // 한 화면에 보여질 컨텐츠 개수
-        slidesToScroll : 1,             //스크롤 한번에 움직일 컨텐츠 개수
+        slidesToScroll : 1,             // 스크롤 한번에 움직일 컨텐츠 개수
         speed : 400,                    // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
         arrows : true,                  // 옆으로 이동하는 화살표 표시 여부
         dots : false,                   // 스크롤바 아래 점으로 페이지네이션 여부
@@ -32,4 +85,4 @@ $(function(){
             }
         ]
     });
-})
+});

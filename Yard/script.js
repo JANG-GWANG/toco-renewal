9a9ca47d1fddcf56ac0c2833185328384edc1383
Jemin-Heo <101089749+Jemin-Heo@users.noreply.github.com>
2022-07-28 19:27:50 +0900

    Add files via upload

--- Yard/script.js
+++ Yard/script.js
@@ -1,32 +1,47 @@
 
  $(document).ready(function(){
-    $('.ham').click(function () {
+    // 
+    $('.ham').click(function() {
         if($('.so').hasClass('more'))
     $('.so').addClass('magictime fadeOut');
     else if($('.so').hasClass('close'))
     $('.so').removeClass('magictime fadeOut');
     });
-     });
-     
-    $(document).ready(function(){
-        $('.ham').click(function(){
-            if($('.so').hasClass('close')){
-                $('.so').addClass('magictime fadeIn');
-                $('.so').addClass('more').removeClass('close');
-            }else if($('.so').hasClass('more')){
-                setTimeout(function() { $('.so').addClass('close').removeClass('more');}, 500);
-            }
-        });
+    // 
+    $('.ham').click(function(){
+        if($('.so').hasClass('close')){
+            $('.so').addClass('magictime fadeIn');
+            $('.so').addClass('more').removeClass('close');
+        }else if($('.so').hasClass('more')){
+            setTimeout(function() { $('.so').addClass('close').removeClass('more');}, 500);
+        }
     });
-
-
-
-$(document).ready(function(){
+    // 
     $('.news-cont').hide();        
-    $('.news-cont').slice(0,6).show();        
-}); 
-function morePage(){
-    $(".news-cont").slice(6,11).show();
-    $(".load").css('display','none');          
-    $(".footer").css('margin-top',"7%"); 
-}    
+    $('.news-cont').slice(0,6).show(); 
+
+    // 
+    $('.load').click(function() {
+        if($('.load').hasClass('firstClick')){
+            $(".news-cont").slice(6,8).show();         
+            $('.load').removeClass('firstClick').addClass('secondClick');            
+        }else if($('.load').hasClass('secondClick')){
+            $(".news-cont").slice(8,10).show();
+            $(".load").removeClass('secondClick');    
+            $(".load").css({'display':'none'}); 
+            $(".footer").css({'margin-top':"7%"});     
+        }  
+    });    
+});    
+
+
+// function morePage(){
+    
+//     $(".news-cont").slice(6,10).show();      
+//     $(".load").css({'display':'none' });           
+//     $(".footer").css({'margin-top':"7%"}); 
+    
+// }    
+
+
+

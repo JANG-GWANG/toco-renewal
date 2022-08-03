5f99b201a3bf711369ae7654ec05e5e3785114d2
JANG-GWANG <103880940+JANG-GWANG@users.noreply.github.com>
2022-08-03 19:48:25 +0900

    Update main.js

--- Main/main.js
+++ Main/main.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
-    
-     $('.slick-header-wrap').slick({
+
+    $('.slick-header-wrap').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
-        arrows: false
+        pauseOnHover:true,
+        arrows: true,
+        infinite:true,
+        speed: 500,
+        prevArrow: $('#prev-arrow'),
+        nextArrow: $('#next-arrow')
     });
-    
+
+
     $(".service-card").on({
         "mouseover":function(){
             $(this).children(".card-show").css({'transform':'translate(-70px, -100px)', 'opacity':'0', 'transition':'0.35s', 'pointer-events':'none'});
-            $(this).children(".card-hide").css({'transform':'translate(0, 0)', 'opacity':'1', 'transition':'0.35s', 'pointer-events':'none'})
+            $(this).children(".card-hide").css({'transform':'translate(0, 0)', 'opacity':'1', 'transition':'0.35s', 'pointer-events':'none'});
         },
         "mouseout":function(){
             $(this).children(".card-show").css({'transform':'translate(0,0)', 'opacity':'1', 'transition':'0.35s', 'pointer-events':'none'});
-            $(this).children(".card-hide").css({'transform':'translate(70px, 100px)', 'opacity':'0', 'transition':'0.35s', 'pointer-events':'none'})
+            $(this).children(".card-hide").css({'transform':'translate(70px, 100px)', 'opacity':'0', 'transition':'0.35s', 'pointer-events':'none'});
+            
         }
     });
 });  

fdd2d0519b371e842ddbbdf721930ce283fee3f7
Jemin-Heo <101089749+Jemin-Heo@users.noreply.github.com>
2022-08-11 08:11:38 +0900

    Add files via upload

--- Yard/script.js
+++ Yard/script.js
@@ -1,6 +1,22 @@
 
  $(document).ready(function(){
     //  네비바 햄버거
+    // $('.ham').click(function() {
+    //     if($('.so').hasClass('more'))
+    // $('.so').addClass('magictime fadeOut');
+    // else if($('.so').hasClass('close'))
+    // $('.so').removeClass('magictime fadeOut');
+    // });
+    // // 
+    // $('.ham').click(function(){
+    //     if($('.so').hasClass('close')){
+    //         $('.so').addClass('magictime fadeIn');
+    //         $('.so').addClass('more').removeClass('close');
+    //     }else if($('.so').hasClass('more')){
+    //         setTimeout(function() { $('.so').addClass('close').removeClass('more');}, 500);
+    //     }
+    // });
+//  네비바 햄버거
     $('.ham').click(function() {
         if($('.so').hasClass('more'))
     $('.so').addClass('magictime fadeOut');
@@ -16,6 +32,7 @@
             setTimeout(function() { $('.so').addClass('close').removeClass('more');}, 500);
         }
     });
+
 
     //  페이지 동작시 콘텐츠 갯수 및 클래스 지정
 

f9ee029905ebbd1b429751a0da03d243e6d26c75
Jemin-Heo <101089749+Jemin-Heo@users.noreply.github.com>
2022-08-12 15:35:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,104 +1,136 @@
-// 메인페이지 메뉴더보기 클릭시 디자인 변경
-$('#menuicon').on('click', function() {
-    if ( $(this).prop('checked') ) {
-        // 메뉴더보기 열릴 때
-        $('.nav-gnb').css({'background-color':'#fff', 'border-bottom':'1px solid #E9E9E9', 'box-sizing':"border-box"});
-        $('.menu-text').css({'color':'#000'});
-        $('.menuicon').css({'background-color':'#000'});
-        $('.menuicon-color').css({'background-color':'#000'});
-        document.getElementById("main-logo").src="./img/toco-common/logo-Resoft.svg";
-        $('.menu-text').hover(function(){
-            $(this).css('color','#184B9F');
-        }, function(){
-            $(this).css('color','#000');
-        });
-    } else {
-        // 메뉴더보기 닫힌 후
-        $('.nav-gnb').css({'background-color':'','border-bottom':'','transition':''});
-        $('.menu-text').css({'color':''});
-        $('.menuicon').css({'background-color':''});
-        $('.menuicon-color').css({'background-color':'', 'transition':'all .35s'});
-        document.getElementById("main-logo").src="./img/toco-common/logo-Resoft-white.png";
-        $('.menu-text').hover(function(){
-            $(this).css('color','#184B9F');
-        }, function(){
-            $(this).css('color','#fff');
-        });
-    }
-  });
 
-
-
-// 다른 페이지 메뉴 마우스 올렸을 때 디자인
-$('#page-menuicon').on('click', function() {
-    if ( $(this).prop('checked') ) {
-        // 메뉴더보기 열릴 때
-        $('.nav-gnb').css({'border-bottom':'1px solid #E9E9E9', 'box-sizing':'border-box'});
-        $('.page-menu-text').hover(function(){
-            $(this).css({'color':'#a0f3ed','transition':'all .35s'});
-        }, function(){
-            $(this).css({'color':'#fff','transition':'all .35s'});
-        });
-    } else {
-        // 메뉴더보기 닫힌 후
-    }
-  });
-
-
-
-// 사업현황페이지 슬릭슬라이더
-$(function(){
-    $(".etc-projects").slick({
-        slide: "div",                   // 슬라이드 되어야 할 태그
-        infinite : true,                // 무한 반복 옵션     
-        slidesToShow : 5,               // 한 화면에 보여질 컨텐츠 개수
-        slidesToScroll : 1,             // 스크롤 한번에 움직일 컨텐츠 개수
-        speed : 400,                    // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-        arrows : true,                  // 옆으로 이동하는 화살표 표시 여부
-        dots : false,                   // 스크롤바 아래 점으로 페이지네이션 여부
-        autoplay : true,                // 자동 스크롤 사용 여부
-        autoplaySpeed : 2000,           // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
-        pauseOnHover : true,            // 슬라이드 이동 시 마우스 호버하면 슬라이더 멈추게 설정
-        vertical : false,               // 세로 방향 슬라이드 옵션
-        prevArrow : false,              // 이전 버튼
-        nextArrow : false,              // 다음 버튼
-        draggable : true,               // 드래그 가능 여부
-        
-        // 반응형 웹 구현 옵션
-        responsive: [
-            {   // 화면크기 1768px 일때
-                breakpoint: 1600,
-                settings: {slidesToShow: 4}
-            },
-            {   // 화면크기 1300px 일때
-                breakpoint: 1300,
-                settings: {slidesToShow: 3}
-            },
-            {   // 화면크기 960px 일때
-                breakpoint: 960,
-                settings: {slidesToShow: 2}
-            }
-        ]
+ $(document).ready(function(){
+  
+//  네비바 햄버거
+    $('.ham').click(function() {
+        if($('.so').hasClass('more'))
+    $('.so').addClass('magictime fadeOut');
+    else if($('.so').hasClass('close'))
+    $('.so').removeClass('magictime fadeOut');
     });
-});
+    // 
+    $('.ham').click(function(){
+        if($('.so').hasClass('close')){
+            $('.so').addClass('magictime fadeIn');
+            $('.so').addClass('more').removeClass('close');
+        }else if($('.so').hasClass('more')){
+            setTimeout(function() { $('.so').addClass('close').removeClass('more');}, 500);
+        }
+    });
+
+
+    //  페이지 동작시 콘텐츠 갯수 및 클래스 지정
+    const newsCont = $('.news-cont');
+    const saveButton = $('.saveButton');
+    const loadButton = $('.loadButton');
+    // function buttonSet(x,y,z){
+    //     newsCont.hide(); 
+    //     saveButton.hide();
+    //     newsCont.slice(x,y).show();  
+    //     if(z<=768){
+    //        let z='firstLoad'
+    //     }else if(768<z<=1024){
+    //        let z='secondLoad'
+    //     }else if(1024<z<1920){
+    //        let z='thirdLoad'
+    //     }else{z=''};
+    //     loadButton.addClass(z); 
+    // }
+    if(window.matchMedia("screen and (max-width: 768px)").matches){
+        
+        $(function(){
+            newsCont.hide(); 
+            saveButton.hide();
+            newsCont.slice(0,4).show();  
+            loadButton.addClass('firstLoad');    
+            // buttonSet(0,4,768);
+            $('.btn-group-vertical>.btn-group:not(:first-child)>.btn, .btn-group-vertical>.btn~.btn').css({'border-top-left-radius':'100%','border-top-right-radius':'100%'})                   
+        });
+    }else if(window.matchMedia("screen and (max-width: 1024px)").matches){
+        $(function(){
+            newsCont.hide(); 
+            newsCont.slice(0,6).show(); 
+            loadButton.addClass('secondLoad');
+            // buttonSet(0,6,1024);
+            saveButton.addClass('thirdSave');
+        });   
+    }else if(window.matchMedia("screen and (max-width: 1920px)").matches){
+        $(function(){
+            newsCont.hide(); 
+            newsCont.slice(0,8).show(); 
+            loadButton.addClass('thirdLoad');
+            // buttonSet(0,8,1920);
+            saveButton.addClass('secondSave');          
+        });       
+    }else{
+        loadButton.hide();            
+        saveButton.addClass('firstSave');
+        $('.btn-group-vertical>.btn-group:not(:last-child)>.btn, .btn-group-vertical>.btn:not(:last-child):not(.dropdown-toggle)').css({'border-bottom-left-radius':'100%','border-bottom-right-radius':'100%'});
+    }
+
+    
+   
+    
+
+    // 펼치기 버튼을 눌렀을때 작동
+    loadButton.click(function() {
+        if(loadButton.hasClass('firstLoad')){
+            const secondPoint = document.getElementById('secondPoint');            
+            newsCont.slice(4,6).show();              
+            saveButton.show();            
+            window.scrollBy({top: secondPoint.getBoundingClientRect().top,behavior:'smooth'});       
+            loadButton.removeClass('firstLoad').addClass('secondLoad');   
+            saveButton.addClass('thirdSave');     
+            $('.btn-group-vertical>.btn-group:not(:first-child)>.btn, .btn-group-vertical>.btn~.btn').css({'border-top-left-radius':'0','border-top-right-radius':'0'}) 
+        }else if(loadButton.hasClass('secondLoad')){
+            const thirdPoint = document.getElementById('thirdPoint');
+            newsCont.slice(6,8).show();
+            saveButton.show();
+            window.scrollBy({top: thirdPoint.getBoundingClientRect().top,behavior:'smooth'});
+            loadButton.removeClass('secondLoad').addClass('thirdLoad'); 
+            saveButton.addClass('secondSave').removeClass('thirdSave');  
+        }else if(loadButton.hasClass('thirdLoad')){
+            const fourthPoint = document.getElementById('fourthPoint');
+            newsCont.slice(8,10).show();
+            saveButton.show();
+            window.scrollBy({top: fourthPoint.getBoundingClientRect().top,behavior:'smooth'});
+            loadButton.removeClass('thirdLoad');  
+            saveButton.addClass('firstSave').removeClass('secondSave');              
+            loadButton.hide();
+            $('.btn-group-vertical>.btn-group:not(:last-child)>.btn, .btn-group-vertical>.btn:not(:last-child):not(.dropdown-toggle)').css({'border-bottom-left-radius':'100%','border-bottom-right-radius':'100%'});
+        };
+    });  
+    // 접기 버튼을 눌렀을때 작동
+    saveButton.click(function() {
+        if(saveButton.hasClass('firstSave')){
+            const thirdPoint = document.getElementById('thirdPoint');
+            window.scrollBy({top: thirdPoint.getBoundingClientRect().top,behavior:'smooth'});
+            newsCont.slice(8,10).hide();
+            loadButton.show();            
+            saveButton.removeClass('firstSave').addClass('secondSave');
+            loadButton.addClass('thirdLoad').removeClass('secondLoad');
+            $(".footer").css({'margin-top':"0"});
+            $('.btn-group-vertical>.btn-group:not(:last-child)>.btn, .btn-group-vertical>.btn:not(:last-child):not(.dropdown-toggle)').css({'border-bottom-left-radius':'0','border-bottom-right-radius':'0'});
+        }else if(saveButton.hasClass('secondSave')){
+            const secondPoint = document.getElementById('secondPoint'); 
+            window.scrollBy({top: secondPoint.getBoundingClientRect().top,behavior:'smooth'}); 
+            newsCont.slice(6,8).hide();
+            loadButton.show();            
+            saveButton.removeClass('secondSave').addClass('thirdSave');  
+            loadButton.addClass('secondLoad').removeClass('firstLoad');                    
+        }else if(saveButton.hasClass('thirdSave')){
+            const firstPoint = document.getElementById('firstPoint'); 
+            window.scrollBy({top: firstPoint.getBoundingClientRect().top,behavior:'smooth'});   
+            newsCont.slice(4,6).hide();
+            loadButton.show();
+            saveButton.hide();
+            saveButton.removeClass('thirdSave');  
+            loadButton.addClass('firstLoad');  
+            $('.btn-group-vertical>.btn-group:not(:first-child)>.btn, .btn-group-vertical>.btn~.btn').css({'border-top-left-radius':'100%','border-top-right-radius':'100%'})                                         
+         };
+    });  
+});    
 
 
 
-// $(document).ready(function(){
-//     $('#page-menuicon').click(function () {
-//         if($('.nav-lnb').hasClass('more'))
-//     $('.nav-lnb').addClass('magictime slideDownReturn');
-//     else if($('.nav-lnb').hasClass('close'))
-//     $('.nav-lnb').removeClass('magictime slideDownReturn');
-// });
-//      });
-// $(document).ready(function(){
-//     $('#page-menuicon').click(function(){
-//         if($('.nav-lnb').hasClass('close')){
-//             $('.nav-lnb').addClass('magictime slideDown');
-//             $('.nav-lnb').addClass('more').removeClass('close');
-//         }else if($('.nav-lnb').hasClass('more')){
-//             setTimeout(function(){$('.nav-lnb').addClass('close').removeClass('more');}, 500);
-//         }
-//     });
-// });
+
